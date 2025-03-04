f8f0b8b00942c706493bcf7784f45657ea06d19d
Bertram Aakjær <89650492+BertramAakjaer@users.noreply.github.com>
2025-03-04 23:58:25 +0100

    fix

--- startup.bat
+++
@@ -1,2 +0,0 @@
-start C:\Users\bertr\AppData\Local\Programs\Python\Python313\pythonw.exe "c:\Users\bertr\OneDrive - NEXT Uddannelse København\Skrivebord\Programmer\Virtual-Gun-VG\tray_icon_setup.pyw"
-exit
